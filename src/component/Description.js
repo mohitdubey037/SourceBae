5dd522a37405b74fd9de0a74daa32129f9ffdf3b
Your Name <amitbishtab95@gmail.com>
2021-03-15 10:38:23 +0530

    project added updated

--- src/component/Description.js
+++ src/component/Description.js
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './homepage59r.css';
 import leftImage from '../assests/Images/onlyphoto.png';
-// import DescriptionSelectbtn from './DescriptionSelectbtn';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-import ReactDOM from 'react-dom';
 import TimezoneSelect from 'react-timezone-select';
-
+// import FormLabel from '@material-ui/core/FormLabel';
+// import DescriptionSelectbtn from './DescriptionSelectbtn';
+import LocationSearchInput from './Location';
 
 const Description = () => {
+
     const history = useHistory();
     const [selectedTimezone, setSelectedTimezone] = useState('');
     const [value, setValue] = React.useState('5-10');
@@ -30,11 +29,10 @@ const Description = () => {
     const agencyDetails = async (e) => {
         e.preventDefault();
         const agencytoken = await localStorage.getItem("agencytoken");
-        // console.log({ userId, value, location, expertise, techstack })
-        // console.log(typeof (JSON.stringify(selectedTimezone)))
         const timeValue = JSON.stringify(selectedTimezone)
 
         var myHeaders = new Headers();
+
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("Authorization", `Bearer ${agencytoken}`)
 
@@ -55,7 +53,6 @@ const Description = () => {
                     history.push('/thankyoupage')) : toast.error('Try Again')
             ))
             .catch(error => console.log('error', error));
-
     }
 
     return (
@@ -73,8 +70,6 @@ const Description = () => {
                     </div>
                     <div className="right-inner-container">
                         <div className="right-content right-content-description">
-                            {/* <h1>Register as Client </h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p> */}
                             <form className="form-description">
                                 <div className="inputArea">
                                     <label className="labels" for="Team" name="Team">Team</label><br />
@@ -89,7 +84,10 @@ const Description = () => {
                                 </div>
                                 <div className="inputArea">
                                     <label className="labels" for="location">Location</label><br />
-                                    <input onChange={(e) => setLocation(e.target.value)} type="text" placeholder="e.g- Indore"></input>
+                                    {/* <input onChange={(e) => setLocation(e.target.value)} type="text" placeholder="e.g- Indore">
+
+                                    </input> */}
+                                    <LocationSearchInput className="timezone" onChange={(e) => setLocation(e.target.value)} />
                                 </div>
                                 <div className="inputArea">
                                     <label className="labels" for="Expertise" name="expertise">Expertise</label><br />
