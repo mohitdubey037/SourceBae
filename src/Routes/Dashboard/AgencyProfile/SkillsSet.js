06a66c56c7da1d9200980cdea6e66282df1d8118
mohit dubey <mohitdubey037@gmail.com>
2021-05-28 12:05:28 +0530

    removed skill set button

--- src/Routes/Dashboard/AgencyProfile/SkillsSet.js
+++ src/Routes/Dashboard/AgencyProfile/SkillsSet.js
@@ -313,7 +313,7 @@ function SkillsSet(props) {
                                                 })
                                             }
                                         </div>
-                                        {editStatus && <div className="editButtons">
+                                        {editStatus && (value.title !== 'Language of Content' && value.title !== 'Relevant Exp In Industry') && <div className="editButtons">
                                             <div className={value?.title} onClick={() => { handleAddData(value) }}>
                                                 <i class="fa fa-plus" aria-hidden="true"></i>
                                             </div>
