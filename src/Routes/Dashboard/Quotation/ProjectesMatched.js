ba698988d7879ec491e72988d2627311e6dd22f4
atulbhatt-system32 <atulbhatt98@gmail.com>
2021-06-24 19:27:46 +0530

    hire Developer done

--- src/Routes/Dashboard/Quotation/ProjectesMatched.js
+++ src/Routes/Dashboard/Quotation/ProjectesMatched.js
@@ -46,7 +46,7 @@ function ProjectesMatched() {
 
     const getAllReceivedData = () => {
         setLoading(true)
-        instance.get(`/api/${Role}/projects/all?AgencyId=${agencyId}&projectCurrentStatus=Short Listed`)
+        instance.get(`/api/${Role}/projects/all?AgencyId=${agencyId}&projectMatched=1`)
             .then(response => {
                 setLoading(false);
                 console.log(response);
