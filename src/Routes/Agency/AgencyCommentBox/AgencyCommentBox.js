5961a44ddb55f9e366519dad3a2aba0f40bdcd1d
Mac <mac@Apples-MacBook-Pro.local>
2021-07-19 11:27:48 +0530

    change in quotationDetails

--- src/Routes/Agency/AgencyCommentBox/AgencyCommentBox.js
+++ src/Routes/Agency/AgencyCommentBox/AgencyCommentBox.js
@@ -40,7 +40,7 @@ const AgencyCommentBox = (props) => {
   };
 
   const [quotationRejectionForm, setQuotationRejectionForm] = useState({
-    rejectReasonByAgency : '',
+    rejectReasonByAgency: '',
     agencyId: localStorage.getItem("userId") || "",
     isQuotationAcceptedByAgency: false,
   })
@@ -123,13 +123,19 @@ const AgencyCommentBox = (props) => {
         style={{
           display: "flex",
           flexDirection: "column",
-          border: "2px solid black",
+          border: "1px solid #ddd",
           borderRadius: "8px",
           padding: "1rem",
           margin: "2rem 1rem 1rem 1rem",
           width: "100%",
+          boxShadow: '0 1px 2px 1px rgba(0,0,0,0.2)',
+          height: '400px',
+          overflow: 'scroll',
+          position: 'relative'
         }}
       >
+        <div className="topLine" style={{
+        }}></div>
         {props.isQuotationAcceptedByClient === true ?
           <p>Quotation accepted by client!!.Waiting for your side</p>
           :
@@ -141,19 +147,15 @@ const AgencyCommentBox = (props) => {
                 <>
                   <div style={{ display: "flex", flexDirection: "column" }}>
                     {index.comment && (
-                      <div>
-                        <h5>
-                          <b>Client: </b>
-                          {index.comment}
-                        </h5>
+                      <div className="chatBox" >
+                        <p style={{ backgroundColor: '#93E9FF' }}>{index.comment}</p>
+                        <b>Client </b>
                       </div>
                     )}
                     {index.reply && (
-                      <div>
-                        <h5>
-                          <b>Agency: </b>
-                          {index.reply}
-                        </h5>
+                      <div className="chatBox" style={{ textAlign: 'right', justifyContent: 'flex-end', display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+                        <p style={{ backgroundColor: '#e1f9ff' }}>{index.reply}</p>
+                        <b>Agency </b>
                       </div>
                     )}
                   </div>
