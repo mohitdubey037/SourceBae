ca7b222c6ee4e5b4e1187f6e18075cd2db8194cf
mohit dubey <mohitdubey037@gmail.com>
2021-07-20 18:51:13 +0530

    changed design of hire developer

--- src/Routes/Client/Dashboard/HireDeveloper/HireDeveloper.js
+++ src/Routes/Client/Dashboard/HireDeveloper/HireDeveloper.js
@@ -16,7 +16,7 @@ import Back from '../../../../Components/Back/Back';
 
 const BlueRadio = withStyles({
   root: {
-    color: "#26AFFF",
+    color: "#3A3A3A",
     "&$checked": {
       color: "#26AFFF",
     },
@@ -34,7 +34,7 @@ function HireDeveloper(props) {
     averageBudget: "",
     expectedStartDate: "",
     contractPeriod: "1 Month",
-    clientId:localStorage.getItem("userId")
+    clientId: localStorage.getItem("userId")
   });
   const [billing, setBilling] = useState(1);
   const handleChange = (event) => {
@@ -139,7 +139,7 @@ function HireDeveloper(props) {
   return (
     <>
       <ClientNavbar />
-      <Back name="Hire Developer"/>
+      <Back name="Hire Developer" />
       <div className="mainHireDeveloper">
         {/* <div
           className="backArrow"
@@ -152,14 +152,13 @@ function HireDeveloper(props) {
         <div className="innerHireDeveloper">
           <div className="hireDeveloperForm">
             <div className="hireDeveloperFormInfo">
+              <div className="predilection-strip"></div>
               <h2>predilection</h2>
               <p>
                 Financed new companies and Enterprises are battling to recruit
                 quality engineers quicker. Even in the wake of offering higher
                 remuneration, advantages and investment opportunities, the
-                ability pool stays restricted.{" "}
-              </p>
-              <p>
+                ability pool stays restricted.
                 26,714 natural solicitations including K***book, Dunzo,
                 Lenskart, IndiaGold, medcords, Shopkirana, Infobeans, pepsico,
                 homelane, Razorpay, Townscript, Bewakoof, SAP, Samsung
@@ -167,7 +166,11 @@ function HireDeveloper(props) {
             </div>
 
             <div className="resourceNumber">
-              <p>1. Requirement Name</p>
+              <ul>
+                <li>
+                  Requirement Name
+                </li>
+              </ul>
               <input
                 type="text"
                 name="requirementName"
@@ -178,7 +181,11 @@ function HireDeveloper(props) {
             </div>
 
             <div className="resourceNumber">
-              <p>2. What roles are you looking for?</p>
+              <ul>
+                <li>
+                  What roles are you looking for?
+                </li>
+              </ul>
               <MultiSelect
                 options={options}
                 value={selectedRoles}
@@ -188,7 +195,11 @@ function HireDeveloper(props) {
             </div>
 
             <div className="resourceNumber">
-              <p>3. Number of Resources</p>
+              <ul>
+                <li>
+                  Number of Resources
+                </li>
+              </ul>
               <input
                 type="number"
                 min="1"
@@ -199,7 +210,11 @@ function HireDeveloper(props) {
               />
             </div>
             <div className="resourceNumber">
-              <p>4. Skills Required</p>
+              <ul>
+                <li>
+                  Skills Required
+                </li>
+              </ul>
               {allTechnologies.length > 0 ? (
                 <MultiSelect
                   options={allTechnologies}
@@ -212,7 +227,11 @@ function HireDeveloper(props) {
               )}
             </div>
             <div className="developerExperienceRequired">
-              <p>5. Average Experience</p>
+              <ul>
+                <li>
+                  Average Experience
+                </li>
+              </ul>
               <FormControl component="fieldset">
                 <RadioGroup
                   aria-label="experience"
@@ -220,28 +239,99 @@ function HireDeveloper(props) {
                   value={apiData.experience}
                   onChange={handleChange}
                 >
-                  <FormControlLabel
-                    value="Junior (1-3years)"
-                    control={<BlueRadio />}
-                    label="Junior (1-3years)"
-                  />
-                  <FormControlLabel
-                    value="Mid Range (3-6 years)"
-                    control={<BlueRadio />}
-                    label="Mid Range (3-6 years)"
-                  />
-                  <FormControlLabel
-                    value="Senior (6-9 years)"
-                    control={<BlueRadio />}
-                    label="Senior (6-9 years)"
-                  />
+                  <div className="Junior">
+                    <div>
+                      <FormControlLabel
+                        value="Junior (1-3years)"
+                        control={<BlueRadio />}
+                      />
+                    </div>
+                    <div className="radio-description">
+                      Junior (1 - 3 Years)
+                    </div>
+                  </div>
+
+                  <div className="strip_HireDeveloper left_HireDeveloper">
+
+                  </div>
+
+                  <div className="Mid-Range">
+                    <div>
+                      <FormControlLabel
+                        value="Mid Range (3-6 years)"
+                        control={<BlueRadio />}
+                      />
+                    </div>
+                    <div className="radio-description">
+                      Mid Range ( 3 - 6  Years)
+                    </div>
+                  </div>
+
+                  <div className="strip_HireDeveloper right_HireDeveloper">
+
+                  </div>
+
+                  <div className="Senior">
+                    <div>
+                      <FormControlLabel
+                        value="Senior (6-9 years)"
+                        control={<BlueRadio />}
+                      />
+                    </div>
+                    <div className="radio-description">
+                      Senior ( 6 - 9 Years)
+                    </div>
+                  </div>
+
                 </RadioGroup>
               </FormControl>
             </div>
 
             <div className="preferredBillingMode">
-              <p>6. Preffered Billing</p>
-              <div className="billingOptions">
+              <ul>
+                <li>
+                  Preffered Billing
+                </li>
+              </ul>
+
+              <FormControl component="fieldset">
+                <RadioGroup
+                  aria-label="billing"
+                  name="billing"
+                  value={apiData.experience}
+                  onChange={handleChange}
+                >
+                  <div className="Weekly">
+                    <div>
+                      <FormControlLabel
+                        value="Weekly"
+                        control={<BlueRadio />}
+                      />
+                    </div>
+                    <div className="radio-description">
+                      Weekly
+                    </div>
+                  </div>
+
+                  <div className="billing_strip_HireDeveloper">
+
+                  </div>
+
+                  <div className="Monthly">
+                    <div>
+                      <FormControlLabel
+                        value="Monthly"
+                        control={<BlueRadio />}
+                      />
+                    </div>
+                    <div className="radio-description">
+                      Monthly
+                    </div>
+                  </div>
+                </RadioGroup>
+              </FormControl>
+
+              {/* <div className="billingOptions">
                 <div className="billingButton" onClick={() => changeBilling(1)}>
                   {billing === 1 ? (
                     <i class="fa fa-check-circle" aria-hidden="true"></i>
@@ -256,109 +346,127 @@ function HireDeveloper(props) {
                   <img src={month} alt="hourPrice" />
                   <h6>Monthly</h6>
                 </div>
-              </div>
+              </div> */}
             </div>
 
             <div className="averageBudget">
-              <p>7. Average Budget</p>
+              <ul>
+                <li>
+                  Average Budget
+                </li>
+              </ul>
               {billing === 1 ? (
                 <FormControl component="fieldset">
-                  <RadioGroup
-                    aria-label="averageBudget"
-                    name="averageBudget"
-                    value={apiData.averageBudget}
-                    onChange={handleChange}
-                  >
-                    <FormControlLabel
-                      value="less than $1500"
-                      control={<BlueRadio />}
-                      label="less than $1500"
-                    />
-                    <FormControlLabel
-                      value="$1500-$2500"
-                      control={<BlueRadio />}
-                      label="$1500-$2500"
-                    />
-                    <FormControlLabel
-                      value="$2500-$4000"
-                      control={<BlueRadio />}
-                      label="$2500-$4000"
-                    />
-                    <FormControlLabel
-                      value="More than $4000"
-                      control={<BlueRadio />}
-                      label="More than $4000"
-                    />
-                  </RadioGroup>
+                  <div className="left-margin">
+                    <RadioGroup
+                      aria-label="averageBudget"
+                      name="averageBudget"
+                      value={apiData.averageBudget}
+                      onChange={handleChange}
+                    >
+                      <FormControlLabel
+                        value="less than $1500"
+                        control={<BlueRadio />}
+                        label="less than $1500"
+                      />
+                      <FormControlLabel
+                        value="$1500-$2500"
+                        control={<BlueRadio />}
+                        label="$1500-$2500 Per Month"
+                      />
+                      <FormControlLabel
+                        value="$2500-$4000"
+                        control={<BlueRadio />}
+                        label="$2500-$4000 Per Month"
+                      />
+                      <FormControlLabel
+                        value="More than $4000"
+                        control={<BlueRadio />}
+                        label="More than $4000 Per Month"
+                      />
+                    </RadioGroup>
+                  </div>
                 </FormControl>
               ) : (
                 <FormControl component="fieldset">
-                  <RadioGroup
-                    aria-label="hourlyBudget"
-                    name="averageBudget"
-                    value={apiData.averageBudget}
-                    onChange={handleChange}
-                  >
-                    <FormControlLabel
-                      value="less than $20"
-                      control={<BlueRadio />}
-                      label="less than $20"
-                    />
-                    <FormControlLabel
-                      value="$20-$40"
-                      control={<BlueRadio />}
-                      label="$20-$40"
-                    />
-                    <FormControlLabel
-                      value="$40-$60"
-                      control={<BlueRadio />}
-                      label="$40-$60"
-                    />
-                    <FormControlLabel
-                      value="Above $60"
-                      control={<BlueRadio />}
-                      label="Above $60"
-                    />
-                  </RadioGroup>
+                  <div className="left-margin">
+                    <RadioGroup
+                      aria-label="hourlyBudget"
+                      name="averageBudget"
+                      value={apiData.averageBudget}
+                      onChange={handleChange}
+                    >
+                      <FormControlLabel
+                        value="less than $20"
+                        control={<BlueRadio />}
+                        label="less than $20"
+                      />
+                      <FormControlLabel
+                        value="$20-$40"
+                        control={<BlueRadio />}
+                        label="$20-$40"
+                      />
+                      <FormControlLabel
+                        value="$40-$60"
+                        control={<BlueRadio />}
+                        label="$40-$60"
+                      />
+                      <FormControlLabel
+                        value="Above $60"
+                        control={<BlueRadio />}
+                        label="Above $60"
+                      />
+                    </RadioGroup>
+                  </div>
                 </FormControl>
               )}
             </div>
 
             <div className="startPeriod">
-              <p>8. Start Date</p>
+              <ul>
+                <li>
+                  Start Date
+                </li>
+              </ul>
               <FormControl component="fieldset">
-                <RadioGroup
-                  aria-label="startDate"
-                  name="expectedStartDate"
-                  value={apiData.expectedStartDate}
-                  onChange={handleChange}
-                >
-                  <FormControlLabel
-                    value="Immediately"
-                    control={<BlueRadio />}
-                    label="Immediately"
-                  />
-                  <FormControlLabel
-                    value="in 1 to 2 weeks"
-                    control={<BlueRadio />}
-                    label="in 1 to 2 weeks"
-                  />
-                  <FormControlLabel
-                    value="more than 2 weeks"
-                    control={<BlueRadio />}
-                    label="more than 2 weeks"
-                  />
-                  <FormControlLabel
-                    value="negotiable"
-                    control={<BlueRadio />}
-                    label="negotiable"
-                  />
-                </RadioGroup>
+                <div className="left-margin">
+                  <RadioGroup
+                    aria-label="startDate"
+                    name="expectedStartDate"
+                    value={apiData.expectedStartDate}
+                    onChange={handleChange}
+                  >
+                    <FormControlLabel
+                      value="Immediately"
+                      control={<BlueRadio />}
+                      label="Immediately"
+                    />
+                    <FormControlLabel
+                      value="in 1 to 2 weeks"
+                      control={<BlueRadio />}
+                      label="in 1 to 2 weeks"
+                    />
+                    <FormControlLabel
+                      value="more than 2 weeks"
+                      control={<BlueRadio />}
+                      label="more than 2 weeks"
+                    />
+                    <FormControlLabel
+                      value="negotiable"
+                      control={<BlueRadio />}
+                      label="negotiable"
+                    />
+                  </RadioGroup>
+                </div>
               </FormControl>
             </div>
 
             <div className="contractPeriod">
-              <p>9. Contract Periods</p>
+              <ul>
+                <li>
+                  Contract Periods
+                </li>
+              </ul>
               <select
                 name="contractPeriod"
                 id="contractPeriod"
@@ -372,8 +480,12 @@ function HireDeveloper(props) {
             </div>
 
             <div className="submitBtn">
-              <div></div>
-              <button onClick={handleSubmit}>Submit</button>
+              <div>
+                <div onClick={handleSubmit}>
+                  Submit
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
