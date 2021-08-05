404d97f3cbc9fadb0fa6dfe1b94c473eb6f09707
mohit dubey <mohitdubey037@gmail.com>
2021-08-05 14:55:42 +0530

    added client one hire developer

--- src/Routes/Client/ClientOneHireDeveloper/ClientOneHireDeveloper.js
+++ src/Routes/Client/ClientOneHireDeveloper/ClientOneHireDeveloper.js
@@ -7,7 +7,9 @@ import { useParams, useHistory } from "react-router-dom";
 import * as helper from '../../../shared/helper';
 import Spinner from '../../../Components/Spinner/Spinner';
 import ClientNavbar from '../ClientNavbar';
-
+import Navbar from '../../../Components/ClientNewestDashboard/Navbar/Navbar'
+import PhoneImage from '../../../assets/images/Newestdashboard/Client-one-hire-developer/phone_icon.svg';
+import Group from '../../../assets/images/Newestdashboard/Client-one-hire-developer/Group.svg';
 
 function ClientOneHireDeveloper(props) {
     let { hireDeveloperId } = useParams();
@@ -26,6 +28,7 @@ function ClientOneHireDeveloper(props) {
         instance
             .get(`/api/${Role}/hire-developers/get/${hireDeveloperId}?clientId=${userId}`)
             .then(function (response) {
+                console.log(response);
                 setSingleHiredDeveloper(response);
                 setLoading(false);
             })
@@ -36,65 +39,110 @@ function ClientOneHireDeveloper(props) {
     };
 
 
-    const handleDevelopers = (agencyId)=>{
-      props.history.push({
-        pathname: `/shared-developers/:${hireDeveloperId}/:${agencyId}`,
-        condition: `Client`
-    })
+    const handleDevelopers = (agencyId) => {
+        props.history.push({
+            pathname: `/shared-developers/:${hireDeveloperId}/:${agencyId}`,
+            condition: `Client`
+        })
     }
 
-    useEffect(()=>{
-        console.log(selectedDevelopers,"selected dev")
-    },[selectedDevelopers])
+    useEffect(() => {
+        console.log(selectedDevelopers, "selected dev")
+    }, [selectedDevelopers])
+    useEffect(() => {
+        console.log(singleHiredDeveloper, "selected dev")
+    }, [singleHiredDeveloper])
     useEffect(() => {
         getOneDeveloper();
     }, []);
 
+    const routeDirect = () => {
+        console.log('hi')
+        if (singleHiredDeveloper?.agenciesMatched?.length > 0) {
+            props.history.push({
+                pathname: `/agency-profile:${singleHiredDeveloper?.agenciesMatched[0]._id}`,
+                condition: 'Client'
+            })
+        }
+    }
+
     return (
         <>
-            <ClientNavbar />
+            <Navbar />
             {loading ? <Spinner /> :
                 <>
-                    <div
+                    {/* <div
                         style={{ marginTop: "55px" }}
                         className="backArrow"
                         onClick={() => routerHistory.goBack()}
                     >
                         <i className="fa fa-angle-left" aria-hidden="true"></i>
-                    </div>
+                    </div> */}
                     <div className="respondCards_clientOneHireDeveloper">
 
                         <div className="moreAgencies_clientOneHireDeveloper">
                             <div className="innerMoreAgencies_clientOneHireDeveloper">
-                            {(singleHiredDeveloper?.agenciesMatched?.length > 0) ? 
-                                <>
-                                <div className="moreAgencyHeading">
-                                    <h3>Matched Agencies</h3>
-                                </div>
-                                <div className="moreAgencyList">
-                                    {
-                                        singleHiredDeveloper?.agenciesMatched?.map((agency) => {
-                                            return (
-                                                <div style={{ cursor: 'pointer' }} className="moreAgencyCard">
- 
-                                                    <div className="moreAgencyInfo">
-                                                        <h6>{`${agency?.agencyId?.agencyName}`}</h6>
-                                                        <p>{agency?.agencyId?.agencyPhone}</p>
-                                                        <p>{agency?.agencyId?.agencyEmail}</p>
-                                                    </div>
-                                                    <div className="moreAgencyLogo">
-                                                        <button onClick={()=>handleDevelopers(agency?.agencyId?._id)}>Check Resources</button>
-                                                    </div>
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                </>
-                                :
-                                    <div style={{padding:"1rem"}}><h2>Sorry No Matched Agencies Found.</h2></div>}
+                                {(singleHiredDeveloper?.agenciesMatched?.length > 0) ?
+                                    <>
+                                        <div style={{ paddingBottom: '1%' }} className="moreAgencyHeading">
+                                            <h3>Matched Agencies</h3>
+                                        </div>
+                                        <div className="moreAgencyList new_design_clientOneHireDeveloper">
+                                            {
+                                                singleHiredDeveloper?.agenciesMatched?.map((agency) => {
+                                                    return (
+                                                        <div style={{ cursor: 'pointer', flexDirection: 'column' }} className="moreAgencyCard">
+
+                                                            <div className="moreAgencyInfo">
+                                                                <h6 style={{ width: '70%' }} className="text-center name-Font">{`${agency?.agencyId?.agencyName}`}</h6>
+                                                                <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'space-between', width: '36%', marginLeft: '40px' }}>
+                                                                    <img src={PhoneImage} alt="phone_image" />
+                                                                    <p>{agency?.agencyId?.agencyPhone}</p>
+                                                                </div>
+                                                                <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between', width: '57%', marginLeft: '40px' }}>
+                                                                    <img src={Group} alt="group" />
+                                                                    <p>{agency?.agencyId?.agencyEmail}</p>
+                                                                </div>
+                                                            </div>
+                                                            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                                                <div style={{
+                                                                    background: '#F2F2F2',
+                                                                    border: '1px solid #DCD3D3',
+                                                                    borderRadius: '5.54545px',
+                                                                    padding: '10px',
+                                                                    width: '34%',
+                                                                    marginTop: '20px',
+                                                                    display: 'flex',
+                                                                    justifyContent: 'center',
+                                                                    alignItems: 'center'
+                                                                }} className="moreAgencyLogo">
+                                                                    <button onClick={() => handleDevelopers(agency?.agencyId?._id)}>Check Resources</button>
+                                                                </div>
+                                                                <div style={{
+                                                                    background: '#F2F2F2',
+                                                                    border: '1px solid #DCD3D3',
+                                                                    borderRadius: '5.54545px',
+                                                                    padding: '10px',
+                                                                    width: '34%',
+                                                                    marginTop: '20px',
+                                                                    display: 'flex',
+                                                                    justifyContent: 'center',
+                                                                    alignItems: 'center',
+                                                                    marginLeft: '30px'
+                                                                }} className="moreAgencyLogo">
+                                                                    <button onClick={() => routeDirect()}>Show Details</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    </>
+                                    :
+                                    <div style={{ padding: "1rem" }}><h2>Sorry No Matched Agencies Found.</h2></div>}
                             </div>
-                        </div>           
+                        </div>
                     </div>
                 </>
             }
