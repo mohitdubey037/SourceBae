c3a85d6e09ad03e6e2d90e0e02218c03dbe93594
mohitdubey037 <mohitdubey037@gmail.com>
2021-09-20 11:01:43 +0530

    done first login page

---
+++ src/Routes/Login/ayushmanLogin.js
@@ -0,0 +1,26 @@
+import './ayushmanLogin.css';
+const ayushmanLogin = () => {
+    return (
+        <div class="main_login">
+            <div class="login_card">
+                <div class="login_switch">
+                    <div>
+                        <p>Agency</p>
+                    </div>
+                    <div>
+                        <p>Client</p>
+                    </div>
+                </div>
+                <div class="type_login">
+                    <p>Login as an <span>Agency</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ayushmanLogin; 
+
+
+
+
