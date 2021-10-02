444dc789eb342885ece75f376f4ad086d6b94bb8
mohitdubey037 <mohitdubey037@gmail.com>
2021-10-02 15:14:46 +0530

    done short term

--- src/Routes/Client/Dashboard/ShortTerm/ShortTerm.js
+++ src/Routes/Client/Dashboard/ShortTerm/ShortTerm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from '../../../../Components/ClientNewestDashboard/Navbar/Navbar';
+import clsx from 'clsx';
 import "./ShortTerm.css";
 
 import fixed from "../../../../assets/images/Newestdashboard/Short_Term/payment.svg";
@@ -35,11 +36,10 @@ const useStyles = makeStyles((theme) => ({
 
 const BlueRadio = withStyles({
   root: {
-    color: "#26AFFF",
     "&$checked": {
       color: "#26AFFF",
     },
-    padding: '0'
+    padding: '0',
   },
   checked: {},
 })((props) => <Radio color="default" {...props} />);
@@ -221,6 +221,7 @@ function ShortTerm(props) {
       <div className="mainShortTerm">
         <Back name="Short Term" />
         <div className="innerShortTerm">
+
           <div className="shortTermForm">
             <div className="shortTermHeading">
               <h2>Short Term Projects</h2>
@@ -258,182 +259,188 @@ function ShortTerm(props) {
             })}
           </div>
 
-          <div class="short_howToPay">
-            <div className="shortTermProjectName">
-              <ul>
-                <li>
-                  Choose a name for your project
-                </li>
-              </ul>
-              <div>
-                <input
-                  type="text"
-                  placeholder="Give a name to identity requirement"
-                  name="projectName"
-                  value={apiData.projectName}
-                  onChange={(event) => handleChange(event)}
-                />
-              </div>
-            </div>
-
-            <div className="howToPay">
-              <ul>
-                <li>
-                  How do you want to pay?
-                </li>
-              </ul>
-              <div className="innerHowToPay">
-                <FormControl component="fieldset">
-                  <RadioGroup
-                    className={classes.root}
-                    aria-label="howToPay"
-                    name="projectPaymentModel"
-                    value={apiData.projectPaymentModel}
-                    onChange={(event) => handleChange(event)}
-                  >
-                    <div className="fixedPrice" name="projectPaymentModel">
-                      <FormControlLabel
-                        color="primary"
-                        value="Fixed Price"
-                        control={<BlueRadio className={classes.root} />}
-                      />
-                      <div className="fixedImage">
-                        <img src={fixed} alt="" />
-                      </div>
-                      <div className="fixedContent">
-                        <h6>Pay fixed price</h6>
-                        <p>
-                          Agree on a price and release payemnt when the job is done.
-                          Best for one-off tasks.
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="fixedPrice" name="projectPaymentModel">
-                      <FormControlLabel
-                        value="By Hour"
-                        control={<BlueRadio className={classes.root} />}
-                      />
-                      <div className="fixedImage">
-                        <img src={hour} alt="hour" />
-                      </div>
-                      <div className="fixedContent">
-                        <h6>Pay by the hour</h6>
-                        <p>
-                          Hire based on an hourly rate and pay for hours billed. Best
-                          for ongoing work
-                        </p>
-                      </div>
-                    </div>
-                  </RadioGroup>
-                </FormControl>
-              </div>
-            </div>
-          </div>
-
-          <div className="projectDesc_estimatedBudget">
-            <div className="shortTermProjectDesc">
-              <ul>
-                <li>
-                  Tell us more about your project
-                </li>
-              </ul>
-              <div className="startABit_shortTermProjectDesc">
-                Start with a bit about yourself or your business, and include an
-                overview what you need done.
-              </div>
-              <div style={{ marginTop: '0.5rem' }}>
-                <textarea
-                  cols="30"
-                  rows="6"
-                  type="text"
-                  name="projectDescription"
-                  value={apiData.projectDescription}
-                  onChange={(event) => handleChange(event)}
-                />
-              </div>
-              <div className="wordsLimit">
-                <p>Minimum 100 characters.</p>
-                <p>{words}/100</p>
-              </div>
-            </div>
-
-            <div className="estimatedBudget">
-              <div className="estimatedBudgetText">
+          <div className="left_and_right_side">
+            <div className="left_side_shortTerm">
+              <div className="shortTermProjectName">
                 <ul>
                   <li>
-                    What is your estimated Budget?
+                    Choose a name for your project
                   </li>
                 </ul>
-              </div>
-              <div>
-                <FormControl component="fieldset">
-                  <RadioGroup
-                    aria-label="projectProposalCost"
-                    name="projectProposalCost"
-                    value={apiData.projectProposalCost}
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Give a name to identity requirement"
+                    name="projectName"
+                    value={apiData.projectName}
                     onChange={(event) => handleChange(event)}
-                  >
-                    <FormControlLabel
-                      color="primary"
-                      value="5000"
-                      control={<BlueRadio className={classes.root} />}
-                      label="$5000-$10000"
-                    />
-                    <FormControlLabel
-                      value="10000"
-                      control={<BlueRadio />}
-                      label="$10000-$150000"
-                    />
-                    <FormControlLabel
-                      value="15000"
-                      control={<BlueRadio />}
-                      label="Max $15000"
-                    />
-                  </RadioGroup>
-                </FormControl>
+                  />
+                </div>
+              </div>
+
+              <div className="shortTermProjectDesc">
+                <ul>
+                  <li>
+                    Tell us more about your project
+                  </li>
+                </ul>
+                <div className="startABit_shortTermProjectDesc">
+                  Start with a bit about yourself or your business, and include an
+                  overview what you need done.
+                </div>
+                <div style={{ marginTop: '0.5rem' }}>
+                  <textarea
+                    cols="30"
+                    rows="6"
+                    type="text"
+                    name="projectDescription"
+                    value={apiData.projectDescription}
+                    onChange={(event) => handleChange(event)}
+                  />
+                </div>
+                <div className="wordsLimit">
+                  <p>Minimum 100 characters.</p>
+                  <p>{words}/100</p>
+                </div>
+              </div>
+
+              <div className="shortTermFileUpload">
+                <div className="uploadBlock">
+                  <div className="fileUploadButton">
+                    <FilePicker
+                      extensions={['jpg', 'pdf', 'png', 'jpeg', 'xlsx']}
+                      onChange={(fileObj) => fileHandler(fileObj)}
+                      onError={errMsg => toast.error(errMsg)}
+                    >
+                      <img className="fileUpload_shortTerm" src={FileUploadImage} alt="image" />
+                    </FilePicker>
+                  </div>
+                  <div className="uploadInfo">
+                    <p>{`${projectFiles?.name ?? "Upload an image or a document that might be helpful in explaining your project in brief."}`}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="shortTermOptionSelect">
+                <ul>
+                  <li>
+                    What work do you need to get done?
+                  </li>
+                </ul>
+
+                <div>
+                  <div>
+                    List of all requirements comma(,) separated
+                  </div>
+                </div>
+
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Ex: Blog Section, Dashboard, Admin Panel,etc"
+                    name="projectRequirements"
+                    value={apiData.projectRequirements}
+                    onChange={(event) => handleChange(event)}
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="shortTermFileUpload">
-            <div className="uploadBlock">
-              <div className="fileUploadButton">
-                <FilePicker
-                  extensions={['jpg', 'pdf', 'png', 'jpeg', 'xlsx']}
-                  onChange={(fileObj) => fileHandler(fileObj)}
-                  onError={errMsg => toast.error(errMsg)}
-                >
-                  <img className="fileUpload_shortTerm" src={FileUploadImage} alt="image" />
-                </FilePicker>
+            <div className="right_side_shortTerm">
+
+              <div className="howToPay">
+                <ul>
+                  <li>
+                    How do you want to pay?
+                  </li>
+                </ul>
+                <div className="innerHowToPay">
+                  <FormControl component="fieldset">
+                    <RadioGroup
+                      className={classes.root}
+                      aria-label="howToPay"
+                      name="projectPaymentModel"
+                      value={apiData.projectPaymentModel}
+                      onChange={(event) => handleChange(event)}
+                    >
+                      <div className="fixedPrice" name="projectPaymentModel">
+                        <FormControlLabel
+                          color="primary"
+                          value="Fixed Price"
+                          control={<BlueRadio className={classes.root} />}
+                        />
+                        <div className="fixedImage">
+                          <img src={fixed} alt="" />
+                        </div>
+                        <div className="fixedContent">
+                          <h6>Pay fixed price</h6>
+                          <p>
+                            Agree on a price and release payemnt when the job is done.
+                            Best for one-off tasks.
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="fixedPrice" name="projectPaymentModel">
+                        <FormControlLabel
+                          value="By Hour"
+                          control={<BlueRadio className={classes.root} />}
+                        />
+                        <div className="fixedImage">
+                          <img src={hour} alt="hour" />
+                        </div>
+                        <div className="fixedContent">
+                          <h6>Pay by the hour</h6>
+                          <p>
+                            Hire based on an hourly rate and pay for hours billed. Best
+                            for ongoing work
+                          </p>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                </div>
               </div>
-              <div className="uploadInfo">
-                <p>{`${projectFiles?.name ?? "Upload an image or a document that might be helpful in explaining your project in brief."}`}</p>
+
+              <div className="estimatedBudget">
+                <div className="estimatedBudgetText">
+                  <ul>
+                    <li>
+                      What is your estimated Budget?
+                    </li>
+                  </ul>
+                </div>
+                <div>
+                  <FormControl component="fieldset">
+                    <RadioGroup
+                      aria-label="projectProposalCost"
+                      name="projectProposalCost"
+                      value={apiData.projectProposalCost}
+                      onChange={(event) => handleChange(event)}
+                    >
+                      <FormControlLabel
+                        color="primary"
+                        value="5000"
+                        control={<BlueRadio className={classes.root} />}
+                        label="$5000 - $10000"
+                      />
+                      <FormControlLabel
+                        value="10000"
+                        control={<BlueRadio />}
+                        label="$10000 - $150000"
+                      />
+                      <FormControlLabel
+                        value="15000"
+                        control={<BlueRadio />}
+                        label="Max $15000"
+                        style={{ marginTop: '0.5rem' }}
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className="shortTermOptionSelect">
-            <ul>
-              <li>
-                What work do you need to get done?
-              </li>
-            </ul>
 
-            <div>
-              <div>
-                List of all requirements comma(,) separated
-              </div>
-            </div>
-
-            <div>
-              <input
-                type="text"
-                placeholder="Ex: Blog Section, Dashboard, Admin Panel,etc"
-                name="projectRequirements"
-                value={apiData.projectRequirements}
-                onChange={(event) => handleChange(event)}
-              />
             </div>
           </div>
 
