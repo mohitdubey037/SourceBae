63781ee6ed963dba0829441b179ca20941661626
ashunamdev <ashunamdev7777@gmail.com>
2021-10-07 16:41:32 +0530

    hire developer form 1,2,3 updated

--- src/Routes/Client/Dashboard/HireAgency/HireAgencyForm3.js
+++ src/Routes/Client/Dashboard/HireAgency/HireAgencyForm3.js
@@ -157,53 +157,8 @@ function HireAgencyForm3(props) {
                 <div className="mainHireAgencyForm3">
                     <img className="Image1_hireAgency" src={UpImage} alt="upImage" />
                     <img className="Image2_hireAgency" src={DownImage} alt="downImage" />
-                    <div className="color-div_hireAgencyForm"></div>
-                    <div className="innerHireAgencyForm3">
-                        <div className="techStackFields">
-                            <div className="HireAgencyForm3Heading">
-                                <h2>How can <span> OneSourcing </span> may help you?</h2>
-                            </div>
-
-                            <div className="serivcesHireAgency">
-                                <p className="servicesAgencyHeadingForm3">
-                                    <ul>
-                                        <li>
-                                            Which kind of application or service would you require?
-                                        </li>
-                                    </ul>
-                                </p>
-
-                                <div className="servicesCardsHireAgency">
-                                    {allServices?.length > 0 ? allServices.map((service) => {
-                                        return (
-                                            <div className="tech-container">
-                                                <div className={`${service.serviceName}`} onClick={(event) => handleServices(event)} style={{ backgroundColor: service.selected ? "#707070" : '#white' }} >
-                                                    <img className={`${service.serviceName}`} src={service.serviceIcon} alt="" />
-                                                </div>
-                                                <p className={`${service.serviceName}`} style={{ color: '#707070', fontFamily: "Segoe UI" }}>{`${service.serviceName}`}</p>
-                                            </div>
-                                        )
-                                    })
-                                        :
-                                        <p>Sorry No Data Found.</p>
-                                    }
-                                    {errors.projectServicesRequiredError && (
-                                        <p className="error_hireAgencyForm2 error_hireAgencyForm3">
-                                            {errors.projectServicesRequiredError}
-                                        </p>)}
-                                </div>
-                            </div>
-
-                            <div className="nextbutton" style={{ width: "100%" }}>
-                                <div className="backbutton_hireAgencyForm3" onClick={() => props.history.push(`/hire-agency-form-two:${projectId}`)} style={{ backgroundColor: '#707070' }}>
-                                    Back
-                                </div>
-                                <div onClick={() => handleSubmit()}>
-                                    Submit
-                                </div>
-                            </div>
-                        </div>
-                        {allTechnologies.length > 0 &&
+                    <div className="color-div_hireAgencyForm">
+                    {allTechnologies.length > 0 &&
                             <div className="serviceFieldsOptions newHireAgencyForm3">
                                 <div className="servicesHireAgencyContainer hireAgencyForm3">
                                     <div className="serviceSelectionInput">
@@ -231,6 +186,100 @@ function HireAgencyForm3(props) {
                         }
                     </div>
                     <div className="steps_hireAgencyForm3">
+                        <div style={{width:"30%"}}>
+                            <div >
+                                <p>Step 1</p>
+                            </div>
+                            <div className='color_hireAgencyForm3 green'></div>
+                        </div>
+
+                        <div className="diabled-step_hireAgencyForm" style={{width:"30%"}}>
+                            <div >
+                                <p className="grey-step_hireAgencyForm">Step 2</p>
+                            </div>
+                            <div className='color_hireAgencyForm3 green'></div>
+                        </div>
+
+                        <div style={{width:"30%"}}>
+                            <div >
+                                <p className="grey-step_hireAgencyForm">Step 3</p>
+                            </div>
+                            <div className='color_hireAgencyForm3 green'></div>
+                        </div>
+                    </div>
+                    <div className="innerHireAgencyForm3">
+                        <div className="techStackFields">
+                            <div className="HireAgencyForm3Heading">
+                                <h2>How can <span> OneSourcing </span> may help you?</h2>
+                            </div>
+
+                            <div className="serivcesHireAgency">
+                                <p className="servicesAgencyHeadingForm3">
+                                    <ul>
+                                        <li>
+                                            Which kind of application or service would you require?
+                                        </li>
+                                    </ul>
+                                </p>
+
+                                <div className="servicesCardsHireAgency">
+                                    {allServices?.length > 0 ? allServices.map((service) => {
+                                        return (
+                                            <div className="tech-container">
+                                                <div className={`${service.serviceName}`} onClick={(event) => handleServices(event)}   style={{filter: service.selected ? " invert(90%) sepia(21%) saturate(287%) hue-rotate(150deg) brightness(98%) contrast(98%)":"none"}} >
+                                                    <img className={`${service.serviceName}`} src={service.serviceIcon} alt="" />
+                                                </div>
+                                                <p className={`${service.serviceName}`} style={{ color: '#707070', fontFamily: "Segoe UI" }}>{`${service.serviceName}`}</p>
+                                            </div>
+                                        )
+                                    })
+                                        :
+                                        <p>Sorry No Data Found.</p>
+                                    }
+                                    {errors.projectServicesRequiredError && (
+                                        <p className="error_hireAgencyForm2 error_hireAgencyForm3">
+                                            {errors.projectServicesRequiredError}
+                                        </p>)}
+                                </div>
+                            </div>
+
+                            <div className="nextbutton" style={{ width: "100%" }}>
+                                <div className="backbutton_hireAgencyForm3" onClick={() => props.history.push(`/hire-agency-form-two:${projectId}`)} style={{ backgroundColor: '#707070' }}>
+                                    Back
+                                </div>
+                                <div onClick={() => handleSubmit()}>
+                                    Submit
+                                </div>
+                            </div>
+                        </div>
+                        {/* {allTechnologies.length > 0 &&
+                            <div className="serviceFieldsOptions newHireAgencyForm3">
+                                <div className="servicesHireAgencyContainer hireAgencyForm3">
+                                    <div className="serviceSelectionInput">
+                                        {allTechnologies ? (
+                                            <>
+                                                <p className="uiuxtext">
+                                                    Select Technologies
+                                                </p>
+                                                <MultiSelect
+                                                    options={allTechnologies}
+                                                    value={selected}
+                                                    onChange={setSelected}
+                                                    labelledBy="Select"
+                                                    className="margin-left"
+                                                />
+                                            </>
+                                        ) : null}
+                                        {errors.projectTechnologiesError && (
+                                            <p className="error_hireAgencyForm2 error-select_hireAgencyForm2">
+                                                {errors.projectTechnologiesError}
+                                            </p>)}
+                                    </div>
+                                </div>
+                            </div>
+                        } */}
+                    </div>
+                    {/* <div className="steps_hireAgencyForm3">
                         <div>
                             <div >
                                 <p>Step 1</p>
@@ -251,7 +300,7 @@ function HireAgencyForm3(props) {
                             </div>
                             <div className='color_hireAgencyForm3 green'></div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             }
         </>
