8efee39a2e6f3c8404315e8f983b2fd7f28dcf5e
mohitdubey037 <mohitdubey037@gmail.com>
2021-10-21 12:26:29 +0530

    page was reloading everytime when we visit agency dashboard, this bug was resolved

--- src/Routes/Dashboard/AgencyNewestDashboard.js
+++ src/Routes/Dashboard/AgencyNewestDashboard.js
@@ -14,12 +14,18 @@ import instance from "../../Constants/axiosConstants";
 import * as helper from "../../shared/helper";
 import NotFound from '../../assets/images/Newestdashboard/Not_found/PageNotFound.svg';
 import Navbar from '../../Components/ClientNewestDashboard/Navbar/Navbar';
+import cookie from "react-cookies";
 
 function AgencyNewestDashboard(props) {
     const Role = localStorage.getItem('role');
     const agencyId = localStorage.getItem("userId");
 
+    const isAgencyVerified = cookie.load("isAgencyVerified");
+    const isStepsCompleted = cookie.load("isStepsCompleted");
+
     const [agencyProfileData, setAgencyProfileData] = useState([])
+    const [tempStatus, setTempStatus] = useState(false); /* this is for checking the component first time, if we remove it then the isAgencyVerified and isStepCompleted will take time to store in cookies and hence the check will fail, So don't remove it  */
+    // DON'T REMOVE IT //
     const [allProjects, setAllProjects] = useState([]);
     const [steps, setSteps] = useState(0);
     const [verified, setVerified] = useState(true);
@@ -39,7 +45,6 @@ function AgencyNewestDashboard(props) {
                 console.log(response);
             })
             .catch((err) => {
-                console.log(err);
             });
     };
 
@@ -47,16 +52,18 @@ function AgencyNewestDashboard(props) {
         getAllProjects();
     }, []);
 
-    useEffect(() => {
-        console.log(agencyProfileData);
-    }, [agencyProfileData])
+    // useEffect(() => {
+
+    // }, [agencyProfileData])
 
     const getStepsCompleted = () => {
         instance.get(`api/${Role}/agencies/steps-completed`)
             .then(function (response) {
                 if (response.stepsCompleted === response.totalSteps) {
                     setSteps(-1);
-                    localStorage.setItem('isVerified', true);
+                    cookie.save("isStepsCompleted", true);
+                    setTempStatus(false);
+                    console.log('h2');
                 }
                 else {
                     setSteps(response.stepsCompleted);
@@ -68,11 +75,13 @@ function AgencyNewestDashboard(props) {
             });
     };
 
-    const getAgencyProfile = (agencyId) => {
+    const getAgencyProfile = () => {
         instance
             .get(`/api/${Role}/agencies/get/${agencyId}`)
             .then(function (response) {
-                console.log(response);
+                console.log('h1');
+                cookie.save("isAgencyVerified", response.isAgencyVerified);
+                // setTempStatus(true)
                 setVerified(response.isAgencyVerified);
                 setUserEmailVerified(response.isUserEmailVerified);
                 setUserPhoneVerified(response.isUserPhoneVerified);
@@ -80,8 +89,19 @@ function AgencyNewestDashboard(props) {
     };
 
     useEffect(() => {
-        getStepsCompleted();
-        getAgencyProfile(localStorage.getItem("userId"));
+        console.log(tempStatus);
+    }, [tempStatus])
+
+    useEffect(() => {
+        // console.log(isAgencyVerified)
+        if (isAgencyVerified != 'true') {
+            console.log('object1')
+            getAgencyProfile();
+        }
+        if (isStepsCompleted != 'true') {
+            console.log('object2')
+            getStepsCompleted();
+        }
     }, []);
 
 
@@ -102,7 +122,7 @@ function AgencyNewestDashboard(props) {
     };
 
     const quotation = (link) => {
-        if (!verified || steps !== -1) {
+        if (isAgencyVerified != 'true' || isStepsCompleted != 'true') {
             props.history.push(`${props.history.location.pathname}`);
         }
         else {
@@ -134,7 +154,7 @@ function AgencyNewestDashboard(props) {
                 <Navbar />
                 <div className="content-body">
                     <div className="content-leftBody">
-                        {!(isUserEmailVerified && isUserPhoneVerified) && steps === -1 && (
+                        {!(isUserEmailVerified && isUserPhoneVerified) && isStepsCompleted == 'true' && (
                             <div className="mainUpdateVerify">
                                 <div className="innerMainVerify">
                                     <p>
@@ -147,9 +167,10 @@ function AgencyNewestDashboard(props) {
                                 </div>
                             </div>
                         )}
-                        {(!verified || steps !== -1) && (
+                        {((isAgencyVerified != 'true' || isStepsCompleted != 'true') && tempStatus === false) && (
+                            //  the major reason of applying tempStatus is that at first event when the agency is verified this check is running becoz isAgencyVerified is storing in cookies a little bit later. so initially we are checking with tempStatus i.e it will be false initially and after the api call it will set to true and hence this check will be verified
                             <div className="mainUpdateVerify">
-                                {!verified && steps !== -1 ? (
+                                {isAgencyVerified != 'true' && isStepsCompleted != 'true' ? (
                                     <div className="innerMainVerify">
                                         <p>
                                             Please
@@ -160,68 +181,68 @@ function AgencyNewestDashboard(props) {
                                         </p>
                                     </div>
                                 ) : (
-                                    <div className="innerMainVerify" style={{marginTop: '1rem'}}>
+                                    <div className="innerMainVerify" style={{ marginTop: '1rem' }}>
                                         <p>Please wait for your profile to be verified by us.</p>
                                     </div>
                                 )}
                             </div>
                         )}
-                    <div className={`user-operations ${(!verified || steps !== -1) && "conditional_marginTop"}`}>
-                        <UserOperations
-                            disabled={!verified || steps !== -1}
-                            nextpage={() => quotation("quotation")}
-                            text='Quotation'
-                            img={QuotationIcon} />
+                        <div className={`user-operations ${(isAgencyVerified != 'true' || isStepsCompleted != 'true') && "conditional_marginTop"}`}>
+                            <UserOperations
+                                disabled={isAgencyVerified != 'true' || isStepsCompleted != 'true'}
+                                nextpage={() => quotation("quotation")}
+                                text='Quotation'
+                                img={QuotationIcon} />
 
-                        <UserOperations
-                            disabled={!verified || steps !== -1}
-                            nextpage={() => quotation("add-developer")}
-                            text="Add Developer"
-                            img={addDeveloperIcon} />
+                            <UserOperations
+                                disabled={isAgencyVerified != 'true' || isStepsCompleted != 'true'}
+                                nextpage={() => quotation("add-developer")}
+                                text="Add Developer"
+                                img={addDeveloperIcon} />
 
-                        {agencyProfileData.productId ?
-                            <UserOperations disabled={(!verified || steps !== -1)}
-                                nextpage={() => quotation('product-details')}
-                                text="View Product"
-                                img={ThirdIcon} />
-                            :
-                            <UserOperations disabled={!verified || steps !== -1}
-                                nextpage={() => quotation('Add Your Product')}
-                                text="Add Your Product"
-                                img={ThirdIcon} />
-                        }
-                    </div>
-                    <div className={`${(!verified || steps !== -1) && "conditional_opacity"}`}>
-                        {allProjects?.projects?.length > 0 &&
-                            <div className="graphic">
-                                <div className="graphic-illustration-heading">
-                                    <h6>Project details</h6>
-                                </div>
-                            </div>
-                        }
-                        <div className="user-project_parent">
-                            {/* <div> */}
-                            {allProjects?.projects?.length > 0 ? (
-                                allProjects?.projects?.map((value, index) => {
-                                    return (
-                                        <AgencyProjectCard
-                                            key={index}
-                                            {...value}
-                                        />
-                                    )
-                                })
-                            ) :
-                                <div className={`not_found agencyNewestDashboard`}>
-                                    <img src={NotFound} alt="NotFound" />
-                                    <p className="no_project_found">No Project Found</p>
-                                </div>
+                            {agencyProfileData.productId ?
+                                <UserOperations disabled={(isAgencyVerified != 'true' || isStepsCompleted != 'true')}
+                                    nextpage={() => quotation('product-details')}
+                                    text="View Product"
+                                    img={ThirdIcon} />
+                                :
+                                <UserOperations disabled={isAgencyVerified != 'true' || isStepsCompleted != 'true'}
+                                    nextpage={() => quotation('Add Your Product')}
+                                    text="Add Your Product"
+                                    img={ThirdIcon} />
                             }
                         </div>
+                        <div className={`${(isAgencyVerified != 'true' || isStepsCompleted != 'true') && "conditional_opacity"}`}>
+                            {allProjects?.projects?.length > 0 &&
+                                <div className="graphic">
+                                    <div className="graphic-illustration-heading">
+                                        <h6>Project details</h6>
+                                    </div>
+                                </div>
+                            }
+                            <div className="user-project_parent">
+                                {/* <div> */}
+                                {allProjects?.projects?.length > 0 ? (
+                                    allProjects?.projects?.map((value, index) => {
+                                        return (
+                                            <AgencyProjectCard
+                                                key={index}
+                                                {...value}
+                                            />
+                                        )
+                                    })
+                                ) :
+                                    <div className={`not_found agencyNewestDashboard`}>
+                                        <img src={NotFound} alt="NotFound" />
+                                        <p className="no_project_found">No Project Found</p>
+                                    </div>
+                                }
+                            </div>
+                        </div>
                     </div>
+                    <RightSide />
                 </div>
-                <RightSide />
             </div>
-        </div>
         </div >
     )
 }
