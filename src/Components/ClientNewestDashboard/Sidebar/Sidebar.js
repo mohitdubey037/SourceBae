801ee090a7d663f72896b5e186f8e2ed0bbaa754
mohitdubey037 <mohitdubey037@gmail.com>
2021-11-28 15:02:48 +0530

    done notification module

--- src/Components/ClientNewestDashboard/Sidebar/Sidebar.js
+++ src/Components/ClientNewestDashboard/Sidebar/Sidebar.js
@@ -27,15 +27,40 @@ function Sidebar(props) {
         props.notificationVisible(!isNotification);
     }
 
-
-    useEffect(() => {
+    const handleGetNotification = () => {
         instance.get(`/api/${Role}/notifications/all?type=push`)
             .then(response => {
                 setNotificationData(response);
             })
             .catch(err => {
             })
+    }
+
+
+    useEffect(() => {
+        handleGetNotification();
     }, [])
+
+    const handleNotificationRead = (id) => {
+        console.log(id);
+        if (id != undefined) {
+            instance.patch(`/api/${Role}/notifications/update`, id)
+            .then(response => {
+                handleGetNotification();
+            })
+            .catch(err => {
+
+            })
+        }
+        else {
+            instance.patch(`/api/${Role}/notifications/update`)
+            .then(response => {
+                handleGetNotification();
+            })
+            .catch(err => {
+            })
+        }
+    }
 
     useEffect(() => {
     }, [isNotification]);
@@ -126,32 +151,41 @@ function Sidebar(props) {
             <div className={isNotification ? 'notificationPanel open' : 'notificationPanel'}>
                 <div className="innerNotificationPanel">
                     <div className="notificationsCards">
-                        <div className="closeNotification">
-                            <i onClick={notificationPanel} className="fa fa-times" aria-hidden="true"></i>
-                        </div>
-                        <div className="allNotification">
-                            <div className="allNotificationText">
-                                <p>All Notification</p>
+                        <div className="close_and_all">
+                            <div className="closeNotification">
+                                <i onClick={notificationPanel} className="fa fa-times" aria-hidden="true"></i>
                             </div>
-
+                            <div className="allNotification">
+                                {/* <div className="allNotificationText"> */}
+                                    <p>All Notification</p>
+                                {/* </div> */}
+                            </div>
+                            <div onClick={() => handleNotificationRead()} className="clearAll">
+                                <p>Clear All</p>
+                            </div>
                         </div>
                         <div className="notificationsCard">
                             {
-                                notificationData.map((nd) => {
-                                    return (
-                                        <div className="notificationPoint">
-                                            <div className="notificationPointIn">
+                                notificationData.length > 0 ?
+                                    notificationData.map((nd) => {
+                                        return (
+                                            <div onClick={() => handleNotificationRead(nd?._id)} className={`notificationPoint ${nd?.isNotificationRead && 'conditionalFilter_Sidebar'}`}>
+                                                {/* <div className="notificationPointIn"> */}
                                                 <ul>
-                                                    <li>
-                                                        <p>{nd.notificationTitle}</p>
-                                                        <p>{nd.notificationData}</p>
+                                                    <li className={`notificationPointLi ${nd?.isNotificationRead && 'conditionalColor_Sidebar'}`}>
+                                                        <p>{nd?.notificationTitle}</p>
+                                                        <p>{nd?.notificationData}</p>
                                                     </li>
                                                 </ul>
                                                 {/* <i className="fa fa-times" aria-hidden="true" style={{ paddingRight: "1rem", marginTop: "0.8rem" }}></i> */}
+                                                {/* </div> */}
                                             </div>
-                                        </div>
-                                    )
-                                })
+                                        )
+                                    })
+                                    :
+                                    <div className="no_new_notification">
+                                        <p>No New Notification</p>
+                                    </div>
                             }
                         </div>
                     </div>
