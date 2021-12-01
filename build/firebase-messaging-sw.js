a657bc0ffe047f385298208d1dd9ca434f91cf3f
mohitdubey037 <mohitdubey037@gmail.com>
2021-12-01 16:35:28 +0530

    logo white added

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-undef */
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
+const config = {
+  apiKey: "AIzaSyA0G6lLOIX4oPNgDlDNbohB4UnkgqDakDk",
+  authDomain: "test-5b61a.firebaseapp.com",
+  projectId: "test-5b61a",
+  storageBucket: "test-5b61a.appspot.com",
+  messagingSenderId: "820347917785",
+  appId: "1:820347917785:web:499db5408ebfef595f75c8",
+  measurementId: "G-52KBXT2G6E",
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
