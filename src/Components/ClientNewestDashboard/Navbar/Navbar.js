541d5c7ff294518fabc177768f76a36b2fcd17b4
mohitdubey037 <mohitdubey037@gmail.com>
2021-12-06 14:47:46 +0530

    popover applied on div

--- src/Components/ClientNewestDashboard/Navbar/Navbar.js
+++ src/Components/ClientNewestDashboard/Navbar/Navbar.js
@@ -2,16 +2,38 @@ import React, { useEffect, useState } from 'react';
 import instance from '../../../Constants/axiosConstants';
 import { withRouter } from 'react-router-dom';
 import RouteRedirect from '../../../Utils/RouteRedirect';
+import { Popover } from 'react-tiny-popover';
 import './Navbar.css';
 import { Avatar } from '@material-ui/core';
+import cookie from "react-cookies";
 
 function Navbar(props) {
 
     const logoLink = "https://api.onesourcing.in/media/images/1637044803259.svg";
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
     const url = props.history.location.pathname;
     const Role = localStorage.getItem('role');
     const roleId = localStorage.getItem("userId");
     const [data, setData] = useState({});
+
+    const myProfileHandler = () => {
+        if (Role === "Agency") {
+            props.history.push('/agency-profile')
+        }
+        if (Role === "Client") {
+            props.history.push('/client-profile');
+        }
+    }
+
+    const logoutHandler = () => {
+        // localStorage.removeItem("Authorization");
+        // localStorage.removeItem('role');
+        // localStorage.removeItem('userId');
+        // localStorage.removeItem('toggle');
+        localStorage.clear();
+        cookie.remove("Authorization");
+        props.history.replace('/');
+    }
 
     useEffect(() => {
         if (Role === 'Client') {
@@ -51,9 +73,9 @@ function Navbar(props) {
                 url.includes('agency-list') ||
                 url.includes('/project-details') ||
                 url.includes('/agency-project-details') ||
-                url.includes('/agencyNewestDashboard') || 
+                url.includes('/agencyNewestDashboard') ||
                 url.includes('/agencyNewestAllProject')
-                ) && '1rem',
+            ) && '1rem',
             justifyContent: (url.includes('/clientNewestDashboard') ||
                 url.includes('/agencyNewestDashboard') ||
                 url.includes('/quotation') ||
@@ -75,6 +97,7 @@ function Navbar(props) {
                 ) && 'none'
             }} className="logoLink_navbar">
                 <img onClick={RouteRedirect1} src={logoLink} alt="logo" />
+
             </div>
             <div className="navbar-items">
                 <div style={{ paddingRight: '10px' }} className="username">
@@ -85,13 +108,39 @@ function Navbar(props) {
                 </div>
                 <div className="userprofile-circle nav-left-item" >
                     {Role === 'Agency' ?
-                        <img src={data?.agencyLogo ? data?.agencyLogo : `https://ui-avatars.com/api/?name=${data?.agencyName}`} />
+                        <>
+                            <img onMouseEnter={() => setIsPopoverOpen(true)}
+                                onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+                                src={data?.agencyLogo ? data?.agencyLogo : `https://ui-avatars.com/api/?name=${data?.agencyName}`} />
+
+                            <Popover
+                                // className="popover"
+                                isOpen={isPopoverOpen}
+                                onClickOutside={() => setIsPopoverOpen(false)} 
+                                positions={['top', 'bottom', 'left', 'right:10']} // preferred positions by priority
+                                marginTop={10}
+                                content={
+                                    <div className="popover_div">
+                                        <button onClick={myProfileHandler}>My Profile</button>
+                                        <div className="horizontal_div"></div>
+                                        <button className="logout_button" onClick={logoutHandler}>Logout</button>
+                                    </div>
+                                }
+                            >
+                                {/* <p>i</p> */}
+                                <div></div>
+                                {/* <div className="popover_div">
+                                    <button onClick={myProfileHandler}>My Profile</button>
+                                    <button onClick={logoutHandler}>Logout</button>
+                                </div> */}
+                            </Popover>
+                        </>
                         :
                         <Avatar src={`https://ui-avatars.com/api/?name=${data[0]?.firstName}+${data[0]?.lastName}`} />
                     }
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
