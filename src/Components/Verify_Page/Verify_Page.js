8141eaf5051ab5479fdf982cc449204bf9c424b1
atulbhatt-system32 <atulbhatt98@gmail.com>
2021-12-22 19:19:18 +0530

    build 2 for 22 dec

--- src/Components/Verify_Page/Verify_Page.js
+++ src/Components/Verify_Page/Verify_Page.js
@@ -17,10 +17,9 @@ function VerifyPage(props) {
         instance.get(`api/${Role}/auths/verify/${verificationToken}`)
         .then(res => {
             setLoading(false);
-            props.history.push('/login');
+            props.history.push(`/login:${Role}`);
         })
         .catch(err => {
-            // props.history.push(`/login:${Role}`);
             props.history.push('/page-not-found');
             setLoading(false);
             toast.error(err);
