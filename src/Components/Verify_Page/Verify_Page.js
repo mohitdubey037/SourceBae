876acbe4b20242bfba2babf8777c36d400a6831a
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-01-18 12:23:10 +0530

    Replaced confirm dialog box with custom modal for verify page

--- src/Components/Verify_Page/Verify_Page.js
+++ src/Components/Verify_Page/Verify_Page.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
-import { useParams, useHistory, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Spinner from '../Spinner/Spinner';
 import instance from "../../Constants/axiosConstants";
 import { toast } from "react-toastify";
 import cookie from "react-cookies";
+import CustomModal from "../CustomModal/CustomModal";
 
 function VerifyPage(props) {
 
@@ -12,7 +13,9 @@ function VerifyPage(props) {
     const verificationToken = new URLSearchParams(search).get('token')
     let role = new URLSearchParams(search).get('role');
 
+
     const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         setLoading(true);
@@ -23,19 +26,23 @@ function VerifyPage(props) {
                     cookie.remove("Authorization");
                     localStorage.setItem("role", role);
                 }
-                if (window.confirm("Congrats you are verified!") == true) {
-                    props.history.push(`login/${role}`);
+                if (window.confirm("Congrats you are verified!") === true) {
+                    setShowModal(true);
                 }
             })
             .catch(err => {
+                setShowModal(true);
                 props.history.push('/page-not-found');
                 setLoading(false);
                 toast.error(err);
             })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
-        loading && <Spinner />
+         <>{loading &&<Spinner />}
+         {showModal && <CustomModal showModal = {showModal} message="Congrats you are verified!" action = {()=>props.history.push(`login/${role}`)} buttonText = "Ok"/>}
+         </>
     )
 }
 
