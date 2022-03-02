7a651bb88b2733f74c204b95e381225bdcf545fa
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-03-02 18:35:59 +0530

    current page moved to top

--- src/Routes/Dashboard/ActiveRequirements/ActiveRequirements.js
+++ src/Routes/Dashboard/ActiveRequirements/ActiveRequirements.js
@@ -12,122 +12,120 @@ import { debounce } from 'lodash';
 
 let currentPage = 1;
 export default function ActiveRequirements() {
-  const [requirementsList, setRequirementsList] = useState({ docs: [] });
-  const role = AGENCY;
+    const [requirementsList, setRequirementsList] = useState({ docs: [] });
+    const role = AGENCY;
 
-  const [searchText, setSearchText] = useState('');
+    const [searchText, setSearchText] = useState('');
 
-  const [filterState, setFilterState] = useState({
-    contractPeriod: undefined,
-    budget: undefined,
-    createdWithin: undefined,
-    minBudget: undefined,
-    maxBudget: undefined
-  });
+    const [filterState, setFilterState] = useState({
+        contractPeriod: undefined,
+        budget: undefined,
+        createdWithin: undefined,
+        minBudget: undefined,
+        maxBudget: undefined
+    });
 
-  let currentPage = 1;
+    const hireDevApi = async (isParam = false, val) => {
+        const url = `/api/${role}/hire-developers/all`;
+        const [minBudget, maxBudget] = filterState?.budget?.split('-') ?? [];
+        console.log(minBudget);
+        instance
+            .get(url, {
+                params: isParam
+                    ? {
+                          contractPeriod: filterState.contractPeriod,
+                          createdWithin: filterState.createdWithin,
+                          searchKeyWord: searchText || val,
+                          minBudget,
+                          maxBudget,
+                          page: currentPage
+                      }
+                    : { page: currentPage }
+            })
+            .then((res) => {
+                setRequirementsList((prevState) => ({
+                    ...res,
+                    docs: prevState?.docs
+                        ? [...prevState?.docs, ...res.docs]
+                        : [...res.docs]
+                }));
+            })
+            .catch((err) => {
+                setRequirementsList({ docs: [] });
+            });
+    };
 
-  const hireDevApi = async (isParam = false, val) => {
-    const url = `/api/${role}/hire-developers/all`;
-    const [minBudget, maxBudget] = filterState?.budget?.split('-') ?? []
-    console.log(minBudget)
-    instance
-      .get(url, {
-        params: isParam
-          ? {
-            contractPeriod: filterState.contractPeriod,
-            createdWithin: filterState.createdWithin,
-            searchKeyWord: searchText || val,
-            minBudget,
-            maxBudget,
-            page: currentPage
-          }
-          : { page: currentPage }
-      })
-      .then((res) => {
-        setRequirementsList((prevState) => ({
-          ...res,
-          docs: prevState?.docs
-            ? [...prevState?.docs, ...res.docs]
-            : [...res.docs]
-        }));
-      })
-      .catch((err) => {
-        setRequirementsList({ docs: [] });
-      });
-  };
+    function handlePagination() {
+        currentPage++;
+        hireDevApi(true);
+    }
+    const debounceFn = useCallback(debounce(hireDevApi, 1000), []);
 
-  function handlePagination() {
-    currentPage++;
-    hireDevApi(true);
-  }
-  const debounceFn = useCallback(debounce(hireDevApi, 1000), []);
+    useEffect(() => {
+        hireDevApi();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [role]);
 
-  useEffect(() => {
-    hireDevApi();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [role]);
-
-  return (
-    <>
-      <LNavbar />
-      <PromotionalStrip />
-      <div className="bodyWrapper">
-        <div className="greyCard">
-          <h1 className="heading">Current Requirements</h1>
-          <div className="partition">
-            <div className="listContainer">
-              {requirementsList?.docs?.map((req, index) => (
-                <RequirementsCard
-                  key={req?._id}
-                  data={req}
-                  showButton={false}
-                  buttonTitle={'Apply now'}
-                />
-              ))}
+    return (
+        <>
+            <LNavbar />
+            <PromotionalStrip />
+            <div className="bodyWrapper">
+                <div className="greyCard">
+                    <h1 className="heading">Current Requirements</h1>
+                    <div className="partition">
+                        <div className="listContainer">
+                            {requirementsList?.docs?.map((req, index) => (
+                                <RequirementsCard
+                                    key={req?._id}
+                                    data={req}
+                                    showButton={false}
+                                    buttonTitle={'Apply now'}
+                                />
+                            ))}
+                        </div>
+                        <div className="optionsContainer">
+                            <SearchAndFilter
+                                searchText={searchText}
+                                setSearchText={(val) => {
+                                    setSearchText(val);
+                                    debounceFn(true, val);
+                                }}
+                                filterState={filterState}
+                                setFilterState={setFilterState}
+                                filterApplier={hireDevApi}
+                            />
+                        </div>
+                    </div>
+                    {currentPage < requirementsList.totalPages && (
+                        <div className={`showMorebtn`}>
+                            <Button
+                                name="show more"
+                                buttonExtraStyle={buttonExtraStyle}
+                                buttonTextStyle={buttonTextStyle}
+                                onClick={() => handlePagination()}
+                            />
+                        </div>
+                    )}
+                </div>
             </div>
-            <div className="optionsContainer">
-              <SearchAndFilter
-                searchText={searchText}
-                setSearchText={(val) => {
-                  setSearchText(val);
-                  debounceFn(true, val);
-                }}
-                filterState={filterState}
-                setFilterState={setFilterState}
-                filterApplier={hireDevApi}
-              />
-            </div>
-          </div>
-          {currentPage < requirementsList.totalPages && (
-            <div className={`showMorebtn`}>
-              <Button
-                name="show more"
-                buttonExtraStyle={buttonExtraStyle}
-                buttonTextStyle={buttonTextStyle}
-                onClick={() => handlePagination()}
-              />
-            </div>
-          )}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 const buttonExtraStyle = {
-  background: 'rgba(1, 95, 154, 0.12)',
-  borderRadius: '6px',
-  border: 'none',
-  width: '100px'
+    background: 'rgba(1, 95, 154, 0.12)',
+    borderRadius: '6px',
+    border: 'none',
+    width: '100px'
 };
 
 const buttonTextStyle = {
-  fontFamily: 'Segoe UI',
-  fontStyle: 'normal',
-  fontWeight: 600,
-  fontSize: '12px',
-  lineHeight: '16px',
-  letterSpacing: '0.4px',
-  textTransform: 'capitalize',
-  color: '#015F9A'
+    fontFamily: 'Segoe UI',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: '12px',
+    lineHeight: '16px',
+    letterSpacing: '0.4px',
+    textTransform: 'capitalize',
+    color: '#015F9A'
 };
