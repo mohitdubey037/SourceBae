0e7225e6c791c236560262fa7f7a1be1dd6f41ca
mujahidARS <mujahid.ahamad@robosoftin.com>
2022-03-05 14:03:18 +0530

    loader and no data component added

--- src/Routes/DevelpoperRequest/DevelopersRequest.js
+++ src/Routes/DevelpoperRequest/DevelopersRequest.js
@@ -11,17 +11,19 @@ import ConfirmationModal from './ConfirmationModal';
 import { ACCEPT, DELETE } from './types';
 import { AGENCY } from '../../shared/constants';
 import instance from '../../Constants/axiosConstants';
+import NoDataComponent from '../../Components/NoData/NoDataComponent';
+import Spinner from '../../Components/Spinner/Spinner';
 
 
 let page = 1
 const DevelopersRequest = () => {
-
 
   const clientId = localStorage.getItem('userId') || '';
   const [nextPage, setnextPage] = useState(page)
   const [modalData, setmodalData] = useState({})
   const [confirmationModalType, setconfirmationModalType] = useState('');
   const [fetchedDevelopers, setfetchedDevelopers] = useState([])
+  const [isLoading, setisLoading] = useState(false)
 
   const role = AGENCY;
 
@@ -31,6 +33,7 @@ const DevelopersRequest = () => {
 
 
   const hireDevApi = async () => {
+    setisLoading(true)
     // const url = `/api/${role}/hire-developers/all`;
     const url = `/api/client/hire-developers/all?clientId=61e541916343484c6752efc0&page=${nextPage}`;
     let params = {
@@ -45,7 +48,8 @@ const DevelopersRequest = () => {
       })
       .catch((err) => {
         console.log(err)
-      });
+      })
+      .finally(() => setisLoading(false));
   };
 
   return (
@@ -55,33 +59,44 @@ const DevelopersRequest = () => {
       </div>
       <Back name={'Developer Request'} />
 
-      <div className={styles.ListingContainer}>
-
-        {
-          fetchedDevelopers?.map((devs, index) => (
-            <>
-              <ListingContainer
-                header={devs?.requirementName}
-                onClick={() => setmodalData(devs)}
-              >
-                <DeveloperListingTag label={'developer listing:'} />
-                <SizedBox height={'20px'} />
-                <div className={styles.cardHolder}>
-                  {devs?.developersShared?.map((item) => (
-                    <div className={styles.cardContainer}>
-                      <DeveloperCard
-                        data={item}
-                        onDelete={() =>
-                          setconfirmationModalType(DELETE)
-                        }
-                        onAccept={() =>
-                          setconfirmationModalType(ACCEPT)
-                        }
-                      />
-                    </div>
-                  ))}
-                </div>
-                {/* <div className={styles.showMorebtn}>
+      {
+        isLoading && (nextPage === 1)
+          ? <Spinner />
+          : <div className={styles.ListingContainer}>
+            {
+              fetchedDevelopers?.length
+                ?
+                fetchedDevelopers?.map((devs, index) => (
+                  <>
+                    <SizedBox height={'80px'} />
+                    <ListingContainer
+                      header={devs?.requirementName}
+                      onClick={() => setmodalData(devs)}
+                    >
+                      <DeveloperListingTag label={'developer listing:'} />
+                      <SizedBox height={'20px'} />
+                      {
+                        devs?.developersShared?.length
+                          ?
+                          <div className={styles.cardHolder}>
+                            {devs?.developersShared?.map((item) => (
+                              <div className={styles.cardContainer}>
+                                <DeveloperCard
+                                  data={item}
+                                  onDelete={() =>
+                                    setconfirmationModalType(DELETE)
+                                  }
+                                  onAccept={() =>
+                                    setconfirmationModalType(ACCEPT)
+                                  }
+                                />
+                              </div>
+                            ))}
+                          </div>
+                          :
+                          <div className={styles.noDevsShared} >No developers shared yet:(</div>
+                      }
+                      {/* <div className={styles.showMorebtn}>
                       <Button
                         onClick={() => { }}
                         name={'Load More'}
@@ -89,24 +104,29 @@ const DevelopersRequest = () => {
                         buttonTextStyle={buttonTextStyle}
                       />
                     </div> */}
-              </ListingContainer>
-              {fetchedDevelopers?.length !== (index + 1) && <SizedBox height={'60px'} />}
-            </>
-          ))
-        }
-
-        {
-          nextPage && <div className={styles.showMorebtn}>
-            <Button
-              onClick={hireDevApi}
-              name={'Load More'}
-              buttonExtraStyle={buttonExtraStyle}
-              buttonTextStyle={buttonTextStyle}
-            />
+                    </ListingContainer>
+                  </>
+                ))
+                :
+                <NoDataComponent />
+            }
+            <div className={styles.showMorebtn}>
+              {
+                nextPage &&
+                (
+                  isLoading
+                    ? <Spinner style={{ height: '60px' }} />
+                    : <Button
+                      name={'Load More'}
+                      onClick={hireDevApi}
+                      buttonExtraStyle={buttonExtraStyle}
+                      buttonTextStyle={buttonTextStyle}
+                    />
+                )
+              }
+            </div>
           </div>
-        }
-
-      </div>
+      }
 
       <DetailsModal data={modalData} open={Object.keys(modalData).length} onCloseModal={() => setmodalData({})} />
 
@@ -114,7 +134,7 @@ const DevelopersRequest = () => {
         type={confirmationModalType}
         onCloseModal={() => setconfirmationModalType('')}
       />
-    </div>
+    </div >
   );
 };
 
@@ -137,7 +157,8 @@ const buttonExtraStyle = {
   background: 'rgba(1, 95, 154, 0.12)',
   borderRadius: '6px',
   border: 'none',
-  width: '100px'
+  width: '100px',
+  margin: '16px'
 };
 
 const buttonTextStyle = {
