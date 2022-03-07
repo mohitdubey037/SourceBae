5bc14e0f93649363112031edaaf2e0bbc4b09d8f
mujahidARS <mujahid.ahamad@robosoftin.com>
2022-03-07 17:32:29 +0530

    fixes

--- src/Components/NoData/NoDataComponent.js
+++ src/Components/NoData/NoDataComponent.js
@@ -6,15 +6,13 @@ import SizedBox from '../SizedBox/SizedBox'
 
 export default function NoDataComponent({ style, text, heading }) {
 
-  const body = text || "We can't seem to find what you're looking for."
+  const body = text ?? "We can't seem to find what you're looking for."
   const head = heading ?? "Aww shucks"
 
   return (
     <div style={style} className={styles.mainContainer} >
       <img src={paperPlane} style={{ height: '100px', width: '100px' }} />
-      <SizedBox height={'20px'} />
       <span className={styles.heading} >{head}</span>
-      <SizedBox height={'8px'} />
       <span className={styles.subText} >{body}</span>
     </div>
   )
